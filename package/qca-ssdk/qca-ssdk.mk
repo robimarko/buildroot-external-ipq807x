d959f8e328304d9b5cdc2809e5c163cd158af0db
Robert Marko <robimarko@gmail.com>
2022-09-29 12:19:19 +0200

    package: add SSDK
    Add basic working Qualcomm SSDK, module instalation needs to be refined
    so it can be autoprobed and not have to be manually loaded.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

---
+++ package/qca-ssdk/qca-ssdk.mk
@@ -0,0 +1,49 @@
+################################################################################
+#
+# qca-ssdk
+#
+################################################################################
+
+QCA_SSDK_VERSION = 628b22bc3d5ee81414b75ab3de6a255c82754dec
+QCA_SSDK_SITE = https://git.codelinaro.org/clo/qsdk/oss/lklm/qca-ssdk.git
+QCA_SSDK_SITE_METHOD = git
+QCA_SSDK_LICENSE = Dual BSD/GPL
+
+QCA_SSDK_MODULE_MAKE_OPTS = \
+	OS=linux \
+	ARCH=$(KERNEL_ARCH) \
+	KVER=$(shell echo $(LINUX_VERSION_PROBED)) \
+	SYS_PATH=$(LINUX_DIR) \
+	TOOLPREFIX=$(TOOLCHAIN_EXTERNAL_PREFIX)- \
+	TOOL_PATH=$(HOST_DIR)/bin/ \
+	GCC_VERSION=110200 \
+	CHIP_TYPE=HPPE \
+	PTP_FEATURE=disable \
+	SWCONFIG_FEATURE=disable \
+	EXTRA_CFLAGS=-fno-stack-protector
+
+define QCA_SSDK_INSTALL_STAGING_HEADERS
+	$(INSTALL) -d $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/api $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/ref $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/fal $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/common/*.h $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/sal/os/linux/*.h $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/sal/os/*.h $(STAGING_DIR)/usr/include/qca-ssdk
+	cp -rf $(@D)/include/init $(STAGING_DIR)/usr/include/qca-ssdk
+endef
+
+define QCA_SSDK_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE1) -C $(@D) $(QCA_SSDK_MODULE_MAKE_OPTS)
+endef
+
+define QCA_SSDK_INSTALL_MOD_SYM
+	cp  $(@D)/Module.symvers $(STAGING_DIR)/usr/include/qca-ssdk
+endef
+QCA_SSDK_POST_BUILD_HOOKS += QCA_SSDK_INSTALL_STAGING_HEADERS QCA_SSDK_INSTALL_MOD_SYM
+
+define QCA_SSDK_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/build/bin/qca-ssdk.ko $(TARGET_DIR)/lib/modules/qca-ssdk.ko
+endef
+
+$(eval $(generic-package))
