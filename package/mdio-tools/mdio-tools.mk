7a1661f2af068f0e27f5264b24386544b85ea4d0
Robert Marko <robimarko@gmail.com>
2022-09-29 12:18:33 +0200

    mdio-tools: update to 1.2.0
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- package/mdio-tools/mdio-tools.mk
+++ package/mdio-tools/mdio-tools.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-MDIO_TOOLS_VERSION = 1.1.1
+MDIO_TOOLS_VERSION = 1.2.0
 MDIO_TOOLS_SITE = https://github.com/wkz/mdio-tools/releases/download/$(MDIO_TOOLS_VERSION)
 MDIO_TOOLS_LICENSE = GPL-2.0-only
+MDIO_TOOLS_LICENSE_FILES = COPYING
 MDIO_TOOLS_DEPENDENCIES = libmnl
 
 MDIO_TOOLS_MODULE_SUBDIRS = kernel
