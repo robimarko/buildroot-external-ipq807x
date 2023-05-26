489b5759119f35dbb007479c961f093eb87ae1dd
Robert Marko <robimarko@gmail.com>
2023-05-26 13:15:49 +0200

    ath11k-bdf: use OpenWrt BDF-s
    OpenWrt nicely hosts the BDF-s even with updated regulatory, so lets just
    use them and avoid dealing with them locally.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- package/ath11k-bdf/ath11k-bdf.mk
+++ package/ath11k-bdf/ath11k-bdf.mk
@@ -4,23 +4,24 @@
 #
 ################################################################################
 
-ATH11K_BDF_SITE = $(BR2_EXTERNAL_IPQ807X_PATH)/package/ath11k-bdf/files
-ATH11K_BDF_SITE_METHOD = local
+ATH11K_BDF_VERSION = 0f73d32e641c4f17e64597da0e6c40ed3cbebe69
+ATH11K_BDF_SITE = https://github.com/openwrt/firmware_qca-wireless.git
+ATH11K_BDF_SITE_METHOD = git
 ATH11K_BDF_LICENSE = Proprietary
 
 # Qnap 301W
 ifeq ($(BR2_PACKAGE_ATH11K_BDF_301W),y)
-ATH11K_BDF_IPQ8074_DIR = 301W
+ATH11K_BDF_IPQ8074_FILE = board-qnap_301w.ipq8074
 endif
 
 # Xiaomi AX9000
 ifeq ($(BR2_PACKAGE_ATH11K_BDF_AX9000),y)
-ATH11K_BDF_IPQ8074_DIR = AX9000
+ATH11K_BDF_IPQ8074_FILE = board-xiaomi_ax9000.ipq8074
 endif
 
 define ATH11K_BDF_INSTALL_TARGET_CMD
 	$(INSTALL) -d $(TARGET_DIR)/lib/firmware/ath11k/IPQ8074/hw2.0
-	$(INSTALL) $(@D)/$(ATH11K_BDF_IPQ8074_DIR)/* $(TARGET_DIR)/lib/firmware/ath11k/IPQ8074/hw2.0
+	$(INSTALL) $(@D)/$(ATH11K_BDF_IPQ8074_FILE) $(TARGET_DIR)/lib/firmware/ath11k/IPQ8074/hw2.0/board-2.bin
 endef
 
 define ATH11K_BDF_INSTALL_TARGET_CMDS
